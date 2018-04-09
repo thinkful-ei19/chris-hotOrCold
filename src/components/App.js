7d26494e1518ae1bbfb8500a4f228c36e04af9dd
Christopher <cjszk@hotmail.com>
2018-04-09 14:08:27 -0700

    basic number game working

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import NumberList from './numberList';
@@ -12,18 +13,52 @@ class App extends Component {
     super(props)
 
     let randomNumber = Math.round((Math.random())*100)
-    console.log(randomNumber)
-
-    let currentGuess;
-    let guesses = [];
-
+    console.log(randomNumber);
     this.state = { 
       answer: randomNumber,
-      guesses: guesses
+      guesses: []
     }
+
   }
 
+  guessIt(number) {
+    console.log(this.state.answer);
+    const correctNumber = this.state.answer;
+    let marginError = number - correctNumber;
+    //Make positive to compare
+    if (marginError < 0) {
+      marginError = (marginError * -1);
+    }
+    console.log(marginError);
+    // if (parseInt(number) == parseInt(this.state.answer)) {
+    //   alert('You guessed correctly!')
+    // } else {
+    //   this.state.guesses.push(number);
+    //   alert('Try again!')
+    // }
+    
+    //If within 10 numbers
+    if (marginError <= 10 && marginError !== 0) {
+      alert('Hot')
+    }
+    //If within 20 numbers
+    else if (marginError <= 20 && marginError >= 11) {
+      alert('Warm')
+    }
+    //Else if it matches
+    else if (marginError === 0) {
+      alert('Correct!')
+    }
+    //Else...
+    else {
+      alert('Cold')
+    }
+  }
+  
   render() {
+    const guessNumber = (number) => {
+      this.guessIt(number);
+    }
 
     return (
       <div className="App">
@@ -33,7 +68,7 @@ class App extends Component {
         <div className="App__main container">
           <h1>Hot or Cold</h1>
           <Feedback />
-          <AppForm />
+          <AppForm handleNumberSubmit={guessNumber} />
           <GuessNumber />
           <NumberList />
         </div>
--- src/components/appForm.js
+++ src/components/appForm.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
-function AppForm () {
-    return (
-        
-        <form className="App__form">
-            <input 
-            className="App__input" />
-            <button className="App__submitButton">Submit</button>
-        </form>
-    )
+class AppForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            number: null
+        }
+    }
+
+    render() {
+        return (
+            <form 
+            onSubmit={event => {
+                event.preventDefault();
+                this.submitNumber()
+                }
+            }
+             className="App__form">
+                <input 
+                onChange={event => this.onInputChange(event.target.value)}
+                className="App__input" />
+                <button className="App__submitButton">Submit</button>
+            </form>
+        )
+    }
+    onInputChange(number) {
+        this.setState({number})
+    }
+
+    submitNumber() {
+        this.props.handleNumberSubmit(this.state.number)
+    }
 }
 
 export default AppForm;
